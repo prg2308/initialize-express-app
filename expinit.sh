4621283f90f1e196bd39b4d325eec34a346840ac
Prayag Sharma <85249351+prg2308@users.noreply.github.com>
2021-11-25 01:26:44 +0530

    Updated script

--- expinit.sh
+++ expinit.sh
@@ -4,17 +4,29 @@ if [ -z "$1" ]; then
     echo "No filename specified"
     exit
 fi
+
+#Create a directory
 mkdir "$1"
 cd "$1" || exit
+
+# Initialise and download express mongoose and ejs npm modules
 npm init -y
 npm i express mongoose ejs
+
+# Create directories
 mkdir controllers models public views utils
 cd public || exit
 mkdir css js
 cd ..
+
+#Create .gitignore file
 touch .gitignore
 echo "/node_modules" >>.gitignore
+
+#Create index.js file
 touch index.js
+
+#Javascript for a basic express app and mongoose connection logic
 printf "const express = require('express')
 const app = express()
 const router = express.Router();
@@ -42,9 +54,16 @@ app.use(methodOverride('_method'));
 
 app.listen(3000, () => {
     console.log('Hosted on port 3000')
-})" >>index.js
+})" >>index.js  #Write it into the Javascript file
 
+#Initialize git repository
 git init
+
+#Stage all changes (Created files and folders)
 git add .
+
+#Commit changes
 git commit -m "Initial commit"
+
+#Open repository on vscode
 code .
